e389f17d138925e685601931469e1e8eca465838
Charlotte366 <jc.oren@hotmail.com>
2022-05-18 11:51:31 +0200

    update

--- my-react/src/components/adminpages/admin/Admin.js
+++ my-react/src/components/adminpages/admin/Admin.js
@@ -1,10 +1,20 @@
+import React from "react";
 import Heading from "../../layout/Heading";
+import Background from "../../../images/background.jpg";
 
 export default function AdminLogin() {
  return (
   <>
+<div className="background">
+<div style= {{ backgroundImage: `url(${Background})`}}>
+      Hello World
+    </div>
+    </div>
+  
    <Heading title="Admin" />
-   <p>This is the admin login page</p>
+  
+
+  
   </>
  );
 }
