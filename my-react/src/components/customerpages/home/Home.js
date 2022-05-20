1ac90f87177c1f5425aba72b70648a0e87f1cf10
Charlotte366 <jc.oren@hotmail.com>
2022-05-20 14:19:30 +0200

    update

--- my-react/src/components/customerpages/home/Home.js
+++ my-react/src/components/customerpages/home/Home.js
@@ -5,6 +5,11 @@ import ImgBed from "../../../images/viewbed.jpg";
 import ImgWater from "../../../images/clearwater.jpg";
 import ImgDrizzle from "../../../images/drizzle.jpg";
 import ImgCosy from "../../../images/fireplace.jpg";
+import ImgbedHotel from "../../../images/1.jpg";
+import ImgMagic from "../../../images/2.jpg";
+import ImgHotel from "../../../images/3.jpg";
+import Footer from "../../layout/Footer";
+
 
 export default function Home() {
    return  (
@@ -14,6 +19,8 @@ export default function Home() {
      <img src={Hero} alt="Bryggen Bergen" />
  </div>
 
+ <Heading title="Home" />
+
  <div className="d-flex flex-row img-front">
  <div className="p-2"><img src={ImgBed} alt="A bed with view from the hills"></img></div>
  <div className="p-2"><img src={ImgWater} alt="A bed with view from the hills"></img></div>
@@ -22,9 +29,41 @@ export default function Home() {
  </div>
 
 
+ <div className="intro-container">
+   <div className="vision-h2">
+   <h2>Why Order with Holidaze</h2>
+     </div> 
+     <div className="vision-text">
+       <p>
+       Get accommodation for every 20th
+accommodation you book
+       </p>
+     </div>
+     <div className="vision-text">
+       <p>Free cancellation</p>
+     </div>
+     <div className="vision-text">
+       <p>We are a local tourism agency
+and we know Bergen  </p>
+     </div>
+     </div>
+
+     <div className="d-flex flex-row img-frontbottom">
+ <div className="p-2"><img src={ImgbedHotel} alt="A bed with view from the hills"></img></div>
+ <div className="p-2"><img src={ImgMagic} alt="A bed with view from the hills"></img></div>
+ <div className="p-2"><img src={ImgHotel} alt="A bed with view from the hills"></img></div>
+
+ </div>
+
+ <Footer />
+
+
+ 
+
+
  
  
- <Heading title="Home" />
+
 
  </>
   );
---
+++ my-react/src/components/layout/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+function Footer() {
+    return (
+        <>
+         <div className="d-flex justify-content-center">
+         <div><p>lorem</p>
+        </div>
+         </div>
+      
+   
+        </>
+    )
+}
+
+export default Footer;
