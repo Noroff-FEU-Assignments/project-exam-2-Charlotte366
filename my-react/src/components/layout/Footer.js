a169e906466d970a8e9fe146013b56619fe86fe2
Charlotte366 <jc.oren@hotmail.com>
2022-05-20 14:33:51 +0200

    update

--- my-react/src/components/layout/Footer.js
+++ my-react/src/components/layout/Footer.js
@@ -5,7 +5,15 @@ function Footer() {
     return (
         <>
          <div className="d-flex justify-content-center">
-         <div><p>lorem</p>
+         <div><h4>Contact Information</h4>
+         <div class="contactInfo">
+         <h6>Address:</h6>
+         <h6>Email:</h6>
+         <h6>Phone:</h6>
+         </div>
+
+         
+       
         </div>
          </div>
       
